a661d834ba29feac2d2d7c1ae91bcd032eb24189
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-06-29 15:57:30 -0600

    Add project files.

---
+++ EjercicioProgramacion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioProgramacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Además, en el método Main de la clase Program, sigue los siguientes pasos:
+            //Crea un objeto de la clase Rectangulo llamado rectangulo.
+            Rectangulo rectangulo = new Rectangulo();
+            //Establece los valores de longitud y ancho del rectángulo utilizando las propiedades.
+            rectangulo.Longitud = 45;
+            rectangulo.Ancho = 74;
+            //Calcula el área del rectángulo llamando al método CalcularArea."
+            Console.WriteLine("El area de rectangulo es: "+rectangulo.CalcularArea(45,74));
+            Console.ReadKey();
+            //Muestra el resultado del cálculo del área en la consola.
+            //Recuerda utilizar correctamente la sobrecarga de constructores, la manipulación de objetos y propiedades, y asegúrate de seguir las convenciones de nomenclatura adecuadas.
+        }
+    }
+}
---
+++ EjercicioProgramacion/Rectangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioProgramacion
+{
+    public class Rectangulo
+    {
+        //Dos variables de instancia privadas de tipo double llamadas longitud y ancho.
+        private double longitud;
+        private double ancho;
+
+      
+        //Un constructor sin parámetros que inicialice las variables de instancia a cero.
+        public Rectangulo()
+        {
+
+        }
+       // Un constructor con parámetros que permita establecer los valores de longitud y ancho al momento de crear un objeto de la clase.
+       public Rectangulo(double longitud, double ancho )
+        {
+
+        }
+        //Propiedades públicas Longitud y Ancho que permitan acceder y modificar las variables de instancia.
+        public double Longitud { get; set; }
+        public double Ancho { get; set; }
+        ///Un método público llamado CalcularArea que calcule el área del rectángulo (multiplicando longitud por ancho) y lo devuelva como resultado.
+        public double CalcularArea(double Longitud, double Ancho)
+        {
+            double resultado = Longitud * Ancho;
+
+            return resultado;
+        }
+        
+    }
+}
